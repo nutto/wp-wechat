c5348d01c89271f7a19f4fdccf8c08d5db310c68
fish_ball <57082212@qq.com>
2015-11-08 22:43:02 +0800

    更新了设置模块的架构

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: EC-WeChat
+Plugin Name: WP WeChat
 Plugin URI:  http://http://www.easecloud.cn/
 Description: This Plugin is designed for using wechat API more conveniently
 Version:     0.1
@@ -9,47 +9,119 @@ Author URI:  http://http://www.easecloud.cn/
 License:     GPL2
 License URI: https://www.gnu.org/licenses/gpl-2.0.html
 Domain Path: null
-Text Domain: null
+Text Domain: wp_wechat
 */
+
+define('WXD', 'wp_wechat');
 
 /**
  * 引入类文件
+ * TODO: 存疑，动态引入库是否会有安全性考虑？
  */
-foreach(glob(plugin_dir_path(__FILE__ ).'class/*.class.php') as $f) {
-    include_once $f;
+foreach(glob(__DIR__.'/class/*.class.php') as $class_file) {
+    include_once $class_file;
 }
+
+/**
+ * 翻译支持
+ */
+add_action('plugins_loaded', function() {
+    load_textdomain(WXD, __DIR__.'/languages/zh_CN.mo');
+});
+
 
 // 下面步骤是建立一个配置页面,并且注册好配置项
 
-/** Step 1.*/
-function wechat_plugin_menu() {
+/**
+ * Step 1. Add admin menu for plugin settings
+ */
+add_action('admin_menu', function () {
+
     // 注册配置页面的渲染函数和信息
-    add_options_page( 'ec-Wechat Plugin Options', 'EC Wechat Plugin', 'manage_options', 'ec-wechat', 'wechat_plugin_options' );
+    add_options_page(
+        __('Wechat Options', WXD),                  // page_title
+        __('Wechat Options', WXD),                  // menu_title
+        'manage_options',                           // capability
+        'options-wechat',                           // menu_slug
+        'wechat_plugin_options'                     // function (callback)
+    );
 
-    // 注册配置项函数
-    add_action( 'admin_init', 'register_ec_wechat_plugin_settings' );
-}
-
-/** Step 2 挂上初始化函数 */
-add_action( 'admin_menu', 'wechat_plugin_menu' );
-
-/** Step 3. */
-function wechat_plugin_options() {
-    // 确保权限
-    if ( !current_user_can( 'manage_options' ) )  {
-        wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+    function wechat_plugin_options() { ?>
+        <div class="wrap">
+            <h2><?php _e('Wechat Settings', 'wp_wechat'); ?></h2>
+            <form method="post" action="options.php">
+                <?php settings_fields(WP_Wechat::OPTION_GROUP); ?>
+                <?php do_settings_sections('options-wechat'); ?>
+                <?php submit_button(); ?>
+            </form>
+        </div><?php
     }
 
-    // 引入配置页面
-    include_once plugin_dir_path(__FILE__ ).'/option-page.php';
-}
+});
 
-/** Step 4. */
-function register_ec_wechat_plugin_settings() {
-    // 注册所有的配置项
-    register_setting( 'ec-wechat-plugin-settings-group', 'wx_app_id' );
-    register_setting( 'ec-wechat-plugin-settings-group', 'wx_app_secret' );
-    register_setting( 'ec-wechat-plugin-settings-group', 'wx_self_id' );
-    register_setting( 'ec-wechat-plugin-settings-group', 'wx_token' );
-    register_setting( 'ec-wechat-plugin-settings-group', 'wx_encoding_AES_key' );
-}
+/**
+ * Step 2. Register the plugin settings.
+ * Referring: https://codex.wordpress.org/Settings_API
+ */
+add_action('admin_init', function() {
+
+    add_settings_section(
+        'section-basic',                            // id
+        __('Basic Settings', WXD),                  // title
+        function() {                                // callback
+            _e('Set the basic Wechat account info here.', WXD);
+        },
+        'options-wechat'                            // page
+    );
+
+    // 预留高级选项卡
+//    add_settings_section(
+//        'section-advanced',                         // id
+//        __('Advanced Settings', WXD),       // title
+//        function() {},                              // callback
+//        'options-wechat'                            // page
+//    );
+
+    foreach(WP_Wechat::OPTION_FIELDS as $field_name => $args) {
+
+        register_setting(WP_Wechat::OPTION_GROUP, $field_name);
+
+        // Referring: https://codex.wordpress.org/Function_Reference/add_settings_field
+        add_settings_field(
+            $field_name,                            // id
+            @$args['title'] ?: $field_name,         // title
+            'field_renderer',                       // callback
+            'options-wechat',                       // page
+            'section-basic',                        // section
+            array_merge(array(
+                'field_name' => $field_name,
+                'label_for' => $field_name,
+            ), $args)                               // $args
+        );
+
+        // TODO: 还有 Sanitization 和 Validation 可以完善
+
+    }
+
+    function field_renderer($args) {
+        $field_name = $args['field_name'];
+        $field_title = @$args['title'] ?: $field_name;
+        $field_type = @$args['type'] ?: 'text';
+        $field_class = @$args['class'] ?: 'regular-text ltr';
+        $field_description = @$args['description'] ?: '';
+        ?>
+        <input class="<?php echo $field_class; ?>"
+               type="<?php echo $field_type;?>"
+               id="<?php echo $field_name;?>"
+               value="<?php form_option($field_name);?>"
+               name="<?php echo $field_name;?>"
+               aria-describedby="<?php echo $field_name;?>-description" />
+        <p class="description"
+           id="<?php echo $field_name;?>-description"><?php
+            echo $field_description;
+            ?></p>
+        <?php
+    }
+
+});
+
